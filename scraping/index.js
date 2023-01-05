dd46ce1f77314aa0d1e86b415e9ce0c7758863e7
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-04 22:41:15 -0500

    :art: Formant file scraping

--- scraping/index.js
+++ scraping/index.js
@@ -3,50 +3,50 @@ import { writeFile } from 'node:fs/promises';
 import path from 'node:path';
 
 const URLS = {
-    leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
+  leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/'
 };
 
 async function scrape(url) {
-    const request = await fetch(url);
-    const html = await request.text();
-    return cheerio.load(html);
+  const request = await fetch(url);
+  const html = await request.text();
+  return cheerio.load(html);
 }
 
 async function getLeaderBoard() {
-    const $ = await scrape(URLS.leaderboard);
-    const $rows = $('table tbody tr');
+  const $ = await scrape(URLS.leaderboard);
+  const $rows = $('table tbody tr');
 
-    const LEADERBOARD_SELECTORS = {
-        team: { selector: '.fs-table-text_3', typeOf: 'string' },
-        victories: { selector: '.fs-table-text_4', typeOf: 'number' },
-        loses: { selector: '.fs-table-text_5', typeOf: 'number' },
-        scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
-        concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
-        yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
-        redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
-    }
+  const LEADERBOARD_SELECTORS = {
+    team: { selector: '.fs-table-text_3', typeOf: 'string' },
+    victories: { selector: '.fs-table-text_4', typeOf: 'number' },
+    loses: { selector: '.fs-table-text_5', typeOf: 'number' },
+    scoredGoals: { selector: '.fs-table-text_6', typeOf: 'number' },
+    concededGoals: { selector: '.fs-table-text_7', typeOf: 'number' },
+    yellowCards: { selector: '.fs-table-text_8', typeOf: 'number' },
+    redCards: { selector: '.fs-table-text_9', typeOf: 'number' }
+  }
 
-    const cleanText = text => text
-        .replace(/\t|\n|\s:/g, '')
-        .replace(/.*:/g, ' ')
-        .trim();
+  const cleanText = text => text
+    .replace(/\t|\n|\s:/g, '')
+    .replace(/.*:/g, ' ')
+    .trim();
 
-    const leaderBoardSelectorsEntries = Object.entries(LEADERBOARD_SELECTORS);
-    const leaderboard = [];
-    $rows.each((_, el) => {
-        const $el = $(el);
+  const leaderBoardSelectorsEntries = Object.entries(LEADERBOARD_SELECTORS);
+  const leaderboard = [];
+  $rows.each((_, el) => {
+    const $el = $(el);
 
-        const leardBoardEntries = leaderBoardSelectorsEntries.map(([key, { selector, typeOf }]) => {
-            const rowValue = $el.find(selector).text();
-            const valueCleaned = cleanText(rowValue);
-            const value = (typeOf === 'number')
-                            ? Number(valueCleaned)
-                            : valueCleaned;
-            return [key, value];
-        });
-        leaderboard.push(Object.fromEntries(leardBoardEntries));
+    const leardBoardEntries = leaderBoardSelectorsEntries.map(([key, { selector, typeOf }]) => {
+      const rowValue = $el.find(selector).text();
+      const valueCleaned = cleanText(rowValue);
+      const value = (typeOf === 'number')
+        ? Number(valueCleaned)
+        : valueCleaned;
+      return [key, value];
     });
-    return leaderboard;
+    leaderboard.push(Object.fromEntries(leardBoardEntries));
+  });
+  return leaderboard;
 }
 
 const leaderboard = await getLeaderBoard();
