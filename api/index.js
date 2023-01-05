6b97f8e73a83378eda5cc373c7d653e7ddfbbdae
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-04 22:38:27 -0500

    :sparkles: Create leaderboard endpoint

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import { Hono } from 'hono';
+import leaderboard from '../db/leaderboard.json';
+
+const app = new Hono();
+
+app.get('/', (context) => {
+  return context.json([
+    {
+      endpoint: '/leaderboard',
+      description: 'Returns the leaderboard'
+    }
+  ]);
+});
+
+app.get('/leaderboard', (context) => {
+  return context.json(leaderboard);
+});
+
+export default app;
