f0a362097379c8da962f8ecfde22654d21371dc7
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-07 16:34:57 -0500

    :sparkles: Add new endpoints

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import { Hono } from 'hono';
 import { serveStatic } from 'hono/serve-static.module';
+
+// db
 import leaderboard from '../db/leaderboard.json';
+import teams from '../db/teams.json';
+import presidents from '../db/presidents.json';
 
 const app = new Hono();
 
@@ -8,13 +12,39 @@ app.get('/', (context) => {
   return context.json([
     {
       endpoint: '/leaderboard',
-      description: 'Returns the leaderboard'
+      description: 'Returns Kings League leaderboard'
+    },
+    {
+      endpoint: '/teams',
+      description: 'Returns Kings League teams'
+    },
+    {
+      endpoint: '/presidents',
+      description: 'Returns Kings League presidents'
     }
   ]);
 });
 
+// Endpoints
 app.get('/leaderboard', (context) => {
   return context.json(leaderboard);
+});
+
+app.get('/teams', (context) => {
+  return context.json(teams);
+});
+
+app.get('/presidents', (context) => {
+  return context.json(presidents);
+});
+
+app.get('/presidents/:id', (context) => {
+  const id = context.req.param('id');
+  const foundPresident = presidents.find(president => president.id === id);
+
+  return foundPresident
+    ? context.json(foundPresident)
+    : context.json({ message: 'President not found' }, 404)
 });
 
 app.get('/static/*', serveStatic({ root: './' }));
