5e0c58c92af313a2e5064be3dd39a7cc473f8c2e
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-18 20:15:02 -0500

    :white_check_mark: add test with vitest

--- api/index.test.js
+++ api/index.test.js
@@ -1,12 +1,12 @@
-import { unstable_dev as unstableDev } from "wrangler";
-import { describe, expect, it, beforeAll, afterAll } from "vitest";
+import { unstable_dev as unstableDev } from 'wrangler';
+import { describe, expect, it, beforeAll, afterAll } from 'vitest';
 
-describe("Worker", () => {
+describe('Worker', () => {
   let worker;
 
   beforeAll(async () => {
     worker = await unstableDev(
-      "api/index.js",
+      'api/index.js',
       {},
       { disableExperimentalWarning: true }
     );
@@ -16,11 +16,15 @@ describe("Worker", () => {
     await worker.stop();
   });
 
-  it("should return Hello World", async () => {
+  it('routes should have endpoint and description', async () => {
     const resp = await worker.fetch();
     if (resp) {
-      const text = await resp.text();
-      expect(text).toMatchInlineSnapshot(`"Hello World!"`);
+      const apiRoutes = await resp.json();
+      // verify the response to have the expected format
+      apiRoutes.forEach((endpoint) => {
+        expect(endpoint).toHaveProperty('endpoint');
+        expect(endpoint).toHaveProperty('description');
+      });
     }
   });
 });
