27b2e2dd48bf50bd0cb42e78ca0585e109627de0
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-21 13:50:49 -0500

    :bug: format log on presidents scrapper

--- scraping/presidents.js
+++ scraping/presidents.js
@@ -1,42 +1,50 @@
-import { writeFile, readFile } from 'node:fs/promises'
-import path from 'node:path'
+import { writeFile, readFile } from 'node:fs/promises';
+import path from 'node:path';
+import { logInfo, logSuccess } from './log';
 
-const STATICS_PATH = path.join(process.cwd(), './assets/static/presidents')
-const DB_PATH = path.join(process.cwd(), './db/')
-const RAW_PRESIDENTS = await readFile(`${DB_PATH}/raw-presidents.json`, 'utf-8').then(JSON.parse)
+const STATICS_PATH = path.join(process.cwd(), './assets/static/presidents');
+const DB_PATH = path.join(process.cwd(), './db/');
+const RAW_PRESIDENTS = await readFile(
+  `${DB_PATH}/raw-presidents.json`,
+  'utf-8'
+).then(JSON.parse);
 
 const presidents = await Promise.all(
-  RAW_PRESIDENTS.map(async presidentsInfo => {
-    const { slug: id, title, _links: links } = presidentsInfo;
+  RAW_PRESIDENTS.map(async (presidentInfo) => {
+    const { slug: id, title, _links: links } = presidentInfo;
     const { rendered: name } = title;
 
     const { 'wp:attachment': attachment } = links;
     const { href: imageApiEndpoint } = attachment[0];
 
-    console.log(`> Everything is done! ${name}`);
+    logInfo(`> Fetching attachment for president: ${name}`);
 
-    const responseImageEndpoint = await fetch(imageApiEndpoint)
+    const responseImageEndpoint = await fetch(imageApiEndpoint);
     const data = await responseImageEndpoint.json();
     const [imageInfo] = data;
-    const { guid: { rendered: imageUrl } } = imageInfo;
+    const {
+      guid: { rendered: imageUrl }
+    } = imageInfo;
 
     const fileExtension = imageUrl.split('.').at(-1);
 
-    console.log(`> Everything is done! ${name}`);
+    logInfo(`> Fetching image for president: ${name}`);
 
-    // fetch the image and save it to file system
     const responseImage = await fetch(imageUrl);
     const arrayBuffer = await responseImage.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
-    console.log(`> Writing image to disk ${name}`);
+    logInfo(`> Writing image to disk ${name}`);
     const imageFileName = `${id}.${fileExtension}`;
     await writeFile(`${STATICS_PATH}/${imageFileName}`, buffer);
 
-    console.log(`> Everything is done! ${name}`);
+    logInfo(`> Everything is done! ${name}`);
     return { id, name, image: imageFileName, teamId: 0 };
   })
-);
+)
 
-console.log('> All presidents are done!');
-await writeFile(`${DB_PATH}/presidents.json`, JSON.stringify(presidents, null, 2));
+logSuccess('> All presidents are done!');
+await writeFile(
+  `${DB_PATH}/presidents.json`,
+  JSON.stringify(presidents, null, 2)
+)
