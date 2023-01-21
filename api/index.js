30efb15f17b66ade2feb077de9bf0c868ab0a427
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-01-20 22:22:41 -0500

    :art: remove trailing slash with method

--- api/index.js
+++ api/index.js
@@ -25,11 +25,11 @@ app.get('/', (ctx) =>
   ])
 );
 
-app.get('/leaderboard\\/?', (ctx) => {
+app.get('/leaderboard', (ctx) => {
   return ctx.json(leaderboard);
 });
 
-app.get('/presidents\\/?', (ctx) => {
+app.get('/presidents', (ctx) => {
   return ctx.json(presidents);
 });
 
@@ -42,7 +42,7 @@ app.get('/presidents/:id', (ctx) => {
     : ctx.json({ message: 'President not found' }, 404);
 })
 
-app.get('/teams\\/?', (ctx) => {
+app.get('/teams', (ctx) => {
   return ctx.json(teams);
 });
 
@@ -57,4 +57,12 @@ app.get('/teams/:id', (ctx) => {
 
 app.get('/static/*', serveStatic({ root: './' }));
 
-export default app;
+app.notFound((c) => {
+  const { pathname } = new URL(c.req.url);
+  if (c.req.url.at(-1) === '/') {
+    return c.redirect(pathname.slice(0, -1));
+  }
+  return c.json({ message: 'Not Found' }, 404);
+})
+
+export default app
