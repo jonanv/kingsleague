740a470d1c522e5db154f6ae0b7e26d856191d3e
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-04 21:27:11 -0500

    :sparkles: add service for presidents

---
+++ src/services/presidents.js
@@ -0,0 +1,12 @@
+const api_url = 'https://kingsleague.jonanv.workers.dev';
+
+export const getPresidentById = async ({ id }) => {
+	try {
+		const response = await fetch(`${ api_url }/presidents/${id}`);
+		const president = await response.json();
+		return president;
+	} catch (error) {
+		// Enviar error a servicio de reporte de errores
+		return [];
+	}
+}
