6bb8b849709b003c2b38408edee56867868f886e
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-12 20:49:15 -0500

    :art: improve scrape with or without parameters

--- scraping/index.js
+++ scraping/index.js
@@ -3,12 +3,22 @@ import { getShortNameTeams } from './short_name_teams.js';
 import { getURLTeams } from './url_teams.js';
 import { scrapedAndSave, SCRIPTING } from "./common/utils.js";
 
-for (const infoToScrap of Object.keys(SCRIPTING)) {
-	await scrapedAndSave(infoToScrap);
+// get first parameter from line
+const [, , scrapeParameter] = process.argv;
+console.log('🚀', scrapeParameter);
+
+if (scrapeParameter) {
+	// Scrape specific data
+	await scrapedAndSave(scrapeParameter);
+} else {
+	// Scraping all data
+	for (const infoToScrape of Object.keys(SCRIPTING)) {
+		await scrapedAndSave(infoToScrape);
+	}
+
+	const teamsWithUrl = await getURLTeams();
+	await writeDBFile('teams', teamsWithUrl);
+	
+	// Update file of teams.json with short name of each team
+	await writeDBFile('teams', getShortNameTeams());
 }
-
-const teamsWithUrl = await getURLTeams();
-await writeDBFile('teams', teamsWithUrl);
-
-// Update file of teams.json with short name of each team
-await writeDBFile('teams', getShortNameTeams());
