8228a0d4cb02d72c05d3eb3a4e59c7b41ab85e74
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-12 20:13:39 -0500

    :art: use url teams for scrape index

--- scraping/index.js
+++ scraping/index.js
@@ -1,14 +1,13 @@
 import { writeDBFile } from '../db/index.js';
 import { getShortNameTeams } from './short_name_teams.js';
-import { getTeams } from './teams.js';
+import { getURLTeams } from './url_teams.js';
 import { scrapedAndSave, SCRIPTING } from "./common/utils.js";
 
 for (const infoToScrap of Object.keys(SCRIPTING)) {
-  await scrapedAndSave(infoToScrap);
-  console.log();
+	await scrapedAndSave(infoToScrap);
 }
 
-const teamsWithUrl = await getTeams();
+const teamsWithUrl = await getURLTeams();
 await writeDBFile('teams', teamsWithUrl);
 
 // Update file of teams.json with short name of each team
