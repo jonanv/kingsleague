392071ae43f5287c08ecf4673290d3b9a039112c
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-16 21:30:16 -0500

    :sparkles: add basic SEO optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-// import prefetch from "@astrojs/prefetch";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 const website = "https://kingsleague.dev/";
 
 // https://astro.build/config
 export default defineConfig({
-	site: website,
-	server: {
-		host: true,
-	},
-	integrations: [
-		tailwind(), 
-		// prefetch()
-	],
+  site: website,
+  server: {
+    host: true,
+  },
+  integrations: [
+    tailwind(),
+    prefetch(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
 });
